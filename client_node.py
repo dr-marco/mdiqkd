ef47f40d22f3d5a86c8c2f8907d7779004c0cee4
dr-marco <m.deramundo@studenti.unibs.it>
2023-07-11 16:19:29 +0200

    complete definition of the MDI node with protocol behavior

--- client_node.py
+++ client_node.py
@@ -9,21 +9,27 @@ class clientProtocol(NodeProtocol):
         self.status = status
         self.portNameQ1=port_names[0] #left arm
         self.portNameQ2=port_names[1] #right arm
-        self.portNameC1=port_names[2] #from
-        self.portNameC2=port_names[3] #to
-        self.HbasisList=Random_basis_gen(self.num_bits)
-        self.XbasisList=Random_basis_gen(self.num_bits)
-        self.loc_measRes=[]
+        self.portNameC1=port_names[2] #receive port from mdi
+        self.portNameC2=port_names[3] #from
+        self.portNameC2=port_names[4] #to
+        #self.HbasisList=Random_basis_gen(self.num_bits)  #TODO Random_basis_gen from function module to use
+        #self.XbasisList=Random_basis_gen(self.num_bits)
+        #self.loc_measRes=[]
         self.key=[]
-        self.sourceQList=[]
+        
 
     def start_protocol():
         #do stuff for initialize protocol
+        this.run(init=True)
         return null
 
 
-    def run():
+    def run(init=False):
+        port = self.node.ports[self.portNameQ2] #use right arm as default
+        input_port = self.node.ports[self.portNameC1]
+        output_port = self.node.ports[self.portNameC2]
         #wait for init 
+
         #set if flip bit is required
         #random bit and basis
         #send quit and wait result
--- mdi_node.py
+++ mdi_node.py
@@ -3,35 +3,46 @@ from netsquid.protocols import NodeProtocol
 import mdi_utils
 
 class mdiProtocol(NodeProtocol):
+    max_delay_for_HOM_interference = 1
+
     def __init__(self, node, status=0):
         super().__init__()
-        self.num_bits=num_bits
         self.node = node
-        self.status = status
-        self.portNameQ1=port_names[0] #left arm
-        self.portNameQ2=port_names[1] #right arm
-        self.portNameC1=port_names[2]  #to
-        self.loc_measRes=[]
-        self.key=[]
-        self.sourceQList=[]
+        self.portNameQ1=port_names[0] #quantum left arm
+        self.portNameQ2=port_names[1] #quantum right arm
+        self.portNameC1=port_names[2] #classic public channel
 
 
     def run():
-        left_port = self.node.portNameQ1
-        right_port = self.node.portNameQ2
-        left_busy = False
-        right_busy = False
+        left_port = self.node.ports[self.portNameQ1]
+        right_port = self.node.ports[self.portNameQ2]
+        output_port = self.node.ports[self.portNameC1]
+        left_busy = False #Check the status of the left port, true if a qubit arrives
+        right_busy = False #Check the status of the right port, true if a qubit arrives
         while True:
-            status = yield (self.await_port_input(left_port)) | (self.await_port_input(right_port))
+            # wait for a qubit in one of the two ports
+            status = yield (self.await_port_input(left_port)) | (self.await_port_input(right_port)) 
+            # check if is arrived on the left and then check if the second almost arrives instantly
             if status.first_term.value:
                 left_qubit,  = left_port.rx_input().items
                 left_busy = True
-            if status.second_term.value:
+                yield (self.await_timer(max_delay_for_HOM_interference)) | (self.await_port_input(right_port))
+                if status.second_term.value:
+                    right_qubit, = right_port.rx_input().items
+                    right_busy = True
+            # check if is arrived on the right instead and then check if the left one almost arrives instantly
+            else if status.second_term.value:
                 right_qubit, = right_port.rx_input().items
                 right_busy = True
+                yield self.await_timer(max_delay_for_HOM_interference) | (self.await_port_input(left_port))
+                if status.first_term.value:
+                    left_qubit,  = left_port.rx_input().items
+                    left_busy = True
+            # if both photons arrives almost in the same time, do the HOM interference and publish the MDI result publicly
             if left_busy and right_busy:
-                #do stuff
-                di_measurement(left_qubit,right_qubit)
-                left_busy = False
-                right_busy = False
+                meas_result = di_measurement(left_qubit,right_qubit)
+                output_port.tx_output(meas_result)
+            # reset the two status flag
+            left_busy = False
+            right_busy = False
                 
